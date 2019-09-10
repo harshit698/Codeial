eb44f5aed3c7c596663d09ce852cdd04a3f6f0c7
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-09-10 19:42:20 +0530

    Set up the view engine

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port=8000;
 
 app.use('/',require('./routes'))
 
+// set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
 
     if(err){
