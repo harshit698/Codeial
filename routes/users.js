5decee81c18c29e51af92b447086a0ba04944cf3
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-09-22 18:35:18 +0530

    added session route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const passport=require('passport');
 const usersConrtoller = require('../controllers/users_controller');
 
 router.get('/profile', usersConrtoller.profile);
 
+router.get('/signUp',usersConrtoller.signUp);
+router.get('/signIn',usersConrtoller.signIn);
+
+router.post('/create',usersConrtoller.create);
+
+// use passport as a middleware authentication
+router.post('/create-session',passport.authenticate(
+    'local',
+    {failureRedirect:'/users/sinIn'},
+),usersConrtoller.createSession);
 
 module.exports = router;
