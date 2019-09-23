cd04ce383dd600b971ebeacedbb0889aa15e92e9
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-09-23 11:15:13 +0530

    added mongostore connect-mongo session

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const db = require('./config/mongoose');
 // used for session cookie
 const session = require('express-session');
 const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-
+const passportLocal = require('./config/passport-local-startegy');
+const MongoStore = require('connect-mongo')(session);
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -19,14 +19,11 @@ app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
-
-
-
-
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// mongo store is used to stire the session cookie in the db
 
 app.use(session({
     name: 'codeial',
@@ -36,12 +33,22 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
+    },
+    store:new MongoStore(
+    {
+        mongooseConnection: db,
+        autoRemove:'disabled'
+    },
+    function(err){
+        console.log(err || 'connect-mongodb setup ok');
     }
+    )
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(passport.setAuthenticatedUser);
 // use express router
 app.use('/', require('./routes'));
 
