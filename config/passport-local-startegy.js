1357c540c3e69a55383374624b2c5bf88da4f02b
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-09-23 11:16:26 +0530

    exporting modules

--- config/passport-local-startegy.js
+++ config/passport-local-startegy.js
@@ -46,4 +46,26 @@ passport.deserializeUser(function(id,done){
     })
 })
 
+// check if the user is authenticated
+passport.checkAuthentication=function(req,res,next){
+
+    // if the user is signed in, then pass on the request to the next function(controller's action)
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    // is the user is not signed in
+
+    return res.redirect('/users/signIn');
+
+}
+
+
+passport.setAuthenticatedUser=function(req,res,next){
+    if(req.isAuthenticated()){
+        // req.user contains the current signed in user from the session cookies and we are just sending this to the locals for the views 
+        res.locals.user=req.user;
+    }
+    next();
+}
 module.exports=passport;
