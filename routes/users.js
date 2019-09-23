0b487652d0084389add87fceaee02ed4ac646fe2
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-09-23 11:12:29 +0530

    added all routes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport=require('passport');
 const usersConrtoller = require('../controllers/users_controller');
 
-router.get('/profile', usersConrtoller.profile);
+router.get('/profile',passport.checkAuthentication, usersConrtoller.profile);
 
 router.get('/signUp',usersConrtoller.signUp);
 router.get('/signIn',usersConrtoller.signIn);
@@ -15,5 +15,7 @@ router.post('/create-session',passport.authenticate(
     'local',
     {failureRedirect:'/users/sinIn'},
 ),usersConrtoller.createSession);
+
+router.get('/signOut', usersConrtoller.destroySession);
 
 module.exports = router;
