edcdb6d86b043210b22717db91c6591180744510
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-09-23 11:11:52 +0530

    added signout controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -8,6 +8,11 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
@@ -16,6 +21,11 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
@@ -46,11 +56,10 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    return res.redirect('/');
+    return res.redirect('/users/profile');
 }
 
-// sign in and create a session for the user
-module.exports.createSession = function(req, res){
-    // TODO later
+module.exports.destroySession = function(req, res){
+    req.logout();
     return res.redirect('/');
 }
