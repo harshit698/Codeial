88bc087988ae950c850820f02ede7c027bc13c56
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-09-24 16:40:14 +0530

    added post contoller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,34 @@
+const Posts=require('../models/post');
+
 module.exports.home = function(req, res){
     
     console.log(req.cookies);
+
+    // Posts.find({},function(err,posts){
+    //     if(err)
+    //     {
+    //         console.log("error in fetchng posts from db")
+    //         return;
+    //     }
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         Post_list:posts
+    //     });
+    // });
+
+    Posts.find({}).populate('user').exec(function(err,posts){
+        if(err)
+        {
+            console.log("error in fetchng posts from db")
+            return;
+        }
+        return res.render('home', {
+            title: "Codeial | Home",
+            Post_list:posts
+        });
+    })
     // here it is rendering the view
-    return res.render('home', {
-        title: "Home"
-    });
+    
 }
 
 // module.exports.actionName = function(req, res){}
