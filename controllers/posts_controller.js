66a1b2a3b4b57eed0ce6b2997acd243bf0885951
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-10-02 22:20:29 +0530

    extracting comments model

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
 const Post=require('../models/post');
-
+const Comment=require('../models/comment');
 module.exports.create=function(req,res){
 
     // if(req.isAuthenticated()){
@@ -18,4 +18,22 @@ module.exports.create=function(req,res){
     //     title: "Codeial | Sign In"
     // })
     
+}
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        
+        //.id means converting object id into string
+        if(post.user == req.user.id){
+            post.remove();
+
+            Comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+
+    })
 }
