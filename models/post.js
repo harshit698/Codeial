a6581f90ebfb6305df1fb02e985695c96e55fa96
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-10-02 22:19:08 +0530

    added comments section in posts

--- models/post.js
+++ models/post.js
@@ -8,7 +8,14 @@ const postSchema=new mongoose.Schema({
     user:{
         type:mongoose.Schema.ObjectId,
         ref:'User'
-    }
+    },
+    // include the arrays of ids of all comments in this post schema itself
+
+    comments:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Comment'
+    }]
+
 },
 {
     timestamps: true
