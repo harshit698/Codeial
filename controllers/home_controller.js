6e86613afa0a13b1d5f012b2c0e9bfac74d20d67
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-10-03 02:27:20 +0530

    added users information

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,5 @@
 const Posts=require('../models/post');
-
+const User = require('../models/user');
 module.exports.home = function(req, res){
     
     console.log(req.cookies);
@@ -16,16 +16,25 @@ module.exports.home = function(req, res){
     //     });
     // });
 
-    Posts.find({}).populate('user').exec(function(err,posts){
-        if(err)
-        {
-            console.log("error in fetchng posts from db")
-            return;
+    Posts.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
         }
-        return res.render('home', {
-            title: "Codeial | Home",
-            Post_list:posts
+    })
+    .exec(function(err, posts){
+
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users: users
+            });
         });
+
+       
     })
     // here it is rendering the view
     
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,15 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
+    User.findById(req.params.id, function(err, user){
+        return res.render('user_profile', {
+            title: 'User Profile',
+            profile_user: user
+        });
+    });
+
 }
+
 
 // render the sign up page
 module.exports.signUp = function(req, res){
