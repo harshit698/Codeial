6b1117bfbc500b2bf8e35c8caa8d6379678be9b2
harshitbhatia98 <harshitbhatia28698@gmail.com>
2019-10-26 15:02:14 +0530

    Added jwt

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,28 @@
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/user');
+
+let options = {
+    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey : 'codeial'
+}
+
+passport.use(new JWTStrategy(options,function(jwtPayLoad,done){
+
+    User.findById(jwtPayLoad._id,function(err,user){
+        if (err){
+            console.log('error in finding user from jwt token ');
+            return;
+        };
+
+        if(user){
+            return done(null,user);
+        }else{
+            return done(null,false);
+        }
+    })
+}));
+
+module.exports = passport;
